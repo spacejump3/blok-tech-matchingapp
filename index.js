2312c787b2be8c8884b330ae066af53d114df592
Stephan <stephan.s.p@outlook.com>
2023-03-06 23:26:09 +0100

    404 pagina toegevoegd + dingen geprobeerd
    Ik heb geprobeerd om de title aan te passen per pagina via ejs (spoiler: lukt niet voor geen reden). Ook probeerde ik afbeeldingen te laten werken, maar dat lukt ook niet. Tot slot heb ik een begin gemaakt aan het kijken of data ophalen via javascript objecten lukt, maar ik snap er geen bal van momenteel. Deze week verder hiermee.

--- index.js
+++ index.js
@@ -2,43 +2,51 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-app.set('views', 'view');
+app.set("view engine", "ejs");
+app.set("views", "view");
 
 app.listen(port, () => {
-  console.log("yoyoyoyooooo" + port);
+  console.log("server started on port" + port);
 });
 
 app.get("/home", (req, res) => {
-  res.render('home')
+  res.render("home", { title: "Home"});
 });
 
 app.get("/magic", (req, res) => {
-  res.render('magic')
+  res.render("magic");
 });
 
-app.get("/magiclist", (req, res) => {
-  res.render('magiclist')
-});
+// data voor als je magic hebt gekozen
 
-app.get("*", (req, res) => {
-  res.render('404')
-});
-
-const magicNPCs = [
+const magicNpcList = [
   {
     name: "Wise old Man",
-    description: "He likes to rob banks"
+    description: "He likes to rob banks",
   },
   {
     name: "Jad",
-    description: "Watch out for his fireballs"
+    description: "Watch out for his fireballs",
   },
   {
     name: "Wizard Mizgog",
-    description: "Likes to roam around the wizard's tower"
-  }
+    description: "Still looking for imps",
+  },
 ];
+
+app.get("/magiclist", (req, res) => {
+  res.render("magiclist", { magicNpcList: magicNpcList });
+});
+
+// 404 pagina altijd onderaan
+
+app.get("/*", (req, res) => {
+  res.render("404");
+});
+
+app.get("/*/*", (req, res) => {
+  res.render("404");
+});
