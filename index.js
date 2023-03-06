c063a4849be0a1ffe23337f01640f507c99814e5
Stephan <stephan.s.p@outlook.com>
2023-03-06 16:45:56 +0100

    ejs toegevoegd

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.use(express.static("static"));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 app.set('views', 'view');
-
-// function home (req, res) {
-//   res.render('home', {data: title})
-// }
 
 app.listen(port, () => {
   console.log("yoyoyoyooooo" + port);
@@ -21,17 +16,29 @@ app.get("/home", (req, res) => {
   res.render('home')
 });
 
-app.get("/home/stephan/", (req, res) => {
-  res.send(
-    '<img src="/img/milk.png" width="200"><h1>Hes the one and only <em>Stephan</em></h1>'
-  );
+app.get("/magic", (req, res) => {
+  res.render('magic')
 });
 
-app.get("/home/:user/", (req, res) => {
-  console.log("input from ${req.params.user}");
-  res.send(
-    '<img src="/img/milk.png" width="200"> <h1>This guy ' +
-      req.params.user +
-      " is a fella</h1>"
-  );
+app.get("/magiclist", (req, res) => {
+  res.render('magiclist')
 });
+
+app.get("*", (req, res) => {
+  res.render('404')
+});
+
+const magicNPCs = [
+  {
+    name: "Wise old Man",
+    description: "He likes to rob banks"
+  },
+  {
+    name: "Jad",
+    description: "Watch out for his fireballs"
+  },
+  {
+    name: "Wizard Mizgog",
+    description: "Likes to roam around the wizard's tower"
+  }
+];
